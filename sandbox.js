6693739f685d2c5679e23ed136a12562dc0f689b
AnujRaj <anujraj24go@gmail.com>
2026-04-03 10:04:44 +0530

    learning

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+// const char = 'anuj';
+// console.log(char);
+let name = 'anujraj';
+const circ = (diameter) => {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
+export {};
---
+++ sandbox.ts
@@ -0,0 +1,19 @@
+// const char = 'anuj';
+// console.log(char);
+
+// const input = document.querySelectorAll('input');
+// console.log(input);
+
+// input.forEach(function (input) {
+//     console.log(input);
+// });
+
+export { };
+
+let name = 'anujraj';
+
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+console.log(circ(10));
